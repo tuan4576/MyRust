166bf86469a2403e310a78a6a96cb693609200dc
thanhtuandev <pro03tuan@gmail.com>
2025-03-20 14:17:03 +0700

    varibal

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,23 @@
 //     println!("Hello, world!");
 // }
 
-use std::io;
+// use std::io;
 
-fn main() {
-    println!("Please input your guess.");
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    println!("You guessed: {}", guess);
+// fn main() {
+//     println!("Please input your guess.");
+//     let mut guess = String::new();
+//     io::stdin()
+//         .read_line(&mut guess)
+//         .expect("Failed to read line");
+//     println!("You guessed: {}", guess);
+// }
+fn main(){
+    let tup = (10, 20, 30);
+    let(x,y,z) = tup;
+    let a = [1,2,3,4];
+    // let a  = 10;
+    // let a = a + 5;
+    // let b = 20;
+    // println!("Tổng là : {}", a + b);
+    println!("Tổng là : {}",a[1]);
 }
